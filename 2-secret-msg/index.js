b949e2959979773d2b0ed0a43fe79bf23c7aa9ca
Joan Serra <78106376+serra-joan@users.noreply.github.com>
2023-11-14 18:47:57 +0100

    bucle updated

--- 2-secret-msg/index.js
+++ 2-secret-msg/index.js
@@ -1,14 +1,11 @@
 const MSG = "";
-const countString = MSG.length;
 
 let responseNum = 0;
 let finalResponse = "";
 
-for (let i = 0; i < countString; i++) {
-    const strValue = MSG[i];
-
-    if(strValue === '#') response ++;
-    else if (strValue === '@') response --;
+for (const char in MSG) {
+    if(char === '#') response ++;
+    else if (char === '@') response --;
     else finalResponse += responseNum.toString();
 }
 
